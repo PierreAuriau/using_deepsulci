d8208488a101f3369c7c6778c104fb298b1c11be
Bastien Cagna <bastiencagna@gmail.com>
2021-05-17 16:07:45 +0200

    Modify htlm report

--- scripts/05_build_report.py
+++ scripts/05_build_report.py
@@ -8,6 +8,7 @@ import numpy as np
 def summary_infos(result_f):
     # Load results
     data = np.load(result_f, allow_pickle=True).item()
+    data = {k: np.array(data[k]) for k in data.keys()}
 
     # Get average by label
     ulabels = np.unique(data['label'])
@@ -54,27 +55,40 @@ def html_report(raw_data, data, modelname, cohortname):
     n_subs = len(np.unique(raw_data['graph']))
     n_labels = len(np.unique(raw_data['label']))
 
-    html = "<html><head><title>" + title + "</title></head><body>"
-    html += '<h1>' + title + '</h1><table>'
+    html = "<html><head><title>" + title + "</title>"
+    html += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">'
+    html += '</head><body><h1>' + title + '</h1><table>'
     html += '<tr><th>Model:</th><td>' + modelname + '</td></tr>'
     html += '<tr><th>Test on cohort:</th><td>' + cohortname + '</td></tr>'
     html += '<tr><th>Number of subjects:</th><td>' + str(n_subs) + '</td></tr>'
     html += '<tr><th>Number of labels:</th><td>' + str(n_labels) + '</td></tr>'
+    html += '<tr><th>Overall accuracy:</th><td>{:.01f}%</td></tr>'.format(
+        data['overall_balanced_accuracy'] * 100)
+    html += '<tr><th>Overall ESI:</th><td>{:.01f}%</td></tr>'.format(
+        data['overall_esi'] * 100)
     html += '</table>'
 
     html += '<h2>Metrics by labels</h2>'
-    html += '<table>'
+    html += '<table class="table"><thead><tr>'
+    html += '<th>Label</th><th>Accuracy</th><th>ESI</th></tr></thead><tbody>'
     for lbl in sorted(data['avarage_balanced_accuracy_by_labels'].keys()):
         avg_acc = data['avarage_balanced_accuracy_by_labels'][lbl]
         std_acc = data['deviation_balanced_accuracy_by_labels'][lbl]
         avg_esi = data['avarage_esi_by_labels'][lbl]
         std_esi = data['deviation_esi_by_labels'][lbl]
-        html += '<tr><th>' + lbl + '</th><td>{:.01f}% (+/- {:01f}%)</td>'.\
+        html += '<tr><th>' + str(lbl) + '</th><td>{:.01f}% (+/- {:.01f}%)</td>'.\
             format(avg_acc * 100, std_acc * 100)
-        html += '<td>{:.01f}% (+/- {:01f}%)</td><tr>'.\
+        html += '<td>{:.01f}% (+/- {:.01f}%)</td><tr>'.\
             format(avg_esi * 100, std_esi * 100)
-    html += '</table>'
+    html += '</tbody></table>'
 
+    html += '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ' \
+            'integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+' \
+            'IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>'
+    html += '<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/' \
+            'js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12' \
+            'Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="ano' \
+            'nymous"></script>'
     html += "</body>"
     return html
 
@@ -92,9 +106,9 @@ def main():
 
     raw, data = summary_infos(op.join(eval_dir, fname))
     report = html_report(raw, data, modelname, cohortname)
-    with open(op.join(eval_dir, fname[:-3] + ".html"), 'w+') as f:
+    with open(op.join(eval_dir, fname[:-4] + ".html"), 'w+') as f:
         f.write(report)
-    print("HTML report: " + op.join(eval_dir, fname[:-3] + ".html"))
+    print("HTML report: " + op.join(eval_dir, fname[:-4] + ".html"))
 
 
 if __name__ == "__main__":
